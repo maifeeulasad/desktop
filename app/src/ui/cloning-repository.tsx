ba57006bb154b966b77fdfd13e6d658b94d090ce
maifeeulasad <maifeeulasad@gmail.com>
2021-04-01 11:09:43 +0600

    clamping not working

--- app/src/ui/cloning-repository.tsx
+++ app/src/ui/cloning-repository.tsx
@@ -11,23 +11,23 @@ interface ICloningRepositoryProps {
 }
 
 /** The component for displaying a cloning repository's progress. */
-export class CloningRepositoryView extends React.Component<
-  ICloningRepositoryProps,
-  {}
-> {
+export class CloningRepositoryView extends React.Component<ICloningRepositoryProps,
+  {}> {
   public render() {
     /* The progress element won't take null for an answer.
      * Only way to get it to be indeterminate is by using undefined */
     const progressValue = this.props.progress.value || undefined
 
     return (
-      <UiView id="cloning-repository-view">
-        <div className="title-container">
+      <UiView id='cloning-repository-view'>
+        <div className='title-container'>
           <Octicon symbol={OcticonSymbol.desktopDownload} />
-          <div className="title">Cloning {this.props.repository.name}</div>
+          <div title={'Cloning ' + this.props.repository.name} className='title'>
+            Cloning {this.props.repository.name}
+          </div>
         </div>
         <progress value={progressValue} />
-        <div title={this.props.progress.description} className="details">
+        <div title={this.props.progress.description} className='details'>
           {this.props.progress.description}
         </div>
       </UiView>
